7964ba4873f60bd1be6b227ead5cf86af4a45206
ImAkhil24 <akhilbadoni24@gmail.com>
2021-11-19 18:36:34 +0530

    login ui design complete

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,60 @@
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import {Container, CssBaseline, Box, Avatar, Typography, TextField, FormControlLabel, Checkbox, Button} from "@mui/material";
+
 const Login = () => {
   return (
-    <div>
-      Login.
-    </div>
+    <Container >
+      <CssBaseline />
+      <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m:1, bgcolor: "secondary.main"}} >
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form">
+            <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            name="email"
+            label="Email Address"
+            autoComplete="email"
+            autoFocus
+            ></TextField>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            {/* <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            /> */}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{mt:2}}
+              >
+                Sign In
+              </Button>
+          </Box>
+        </Box>
+    </Container>
   )
 }
 
