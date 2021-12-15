b0d7c537774a08205404f08c619f77cf048e013a
ImAkhil24 <akhilbadoni24@gmail.com>
2021-12-15 10:54:37 +0530

    added sound notifaction in the timer

--- src/components/PomTimer.jsx
+++ src/components/PomTimer.jsx
@@ -1,12 +1,25 @@
-import { Card, Paper, Box, Typography, Button, CardHeader } from "@mui/material";
+import {
+  Card,
+  Paper,
+  Box,
+  Typography,
+  Button,
+  CardHeader,
+} from "@mui/material";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import StopIcon from '@mui/icons-material/Stop';
+import StopIcon from "@mui/icons-material/Stop";
 import { useState } from "react";
+import soundFile from "../sound.wav";
+import { useRef } from "react";
+// import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+
 // a circle inside which the timer will be displayed
 // a session time button with + and - signs to increment by 60 and decrement by 60
 // a break time button also
 
 const PomTimer = () => {
+  const soundElement = useRef(null);
+
   // states
 
   const initialSessionDetail = {
@@ -16,109 +29,113 @@ const PomTimer = () => {
     currentBreakTime: 10,
     active: "false", // false break session
     timer: null,
-  }
+  };
 
   const [sessionDetail, setsessionDetail] = useState(initialSessionDetail);
 
-
   // handlers
   const sessionIncrementHandler = (e) => {
-    setsessionDetail((prevState)=>{
-      let {sessionTime} = prevState;
-      sessionTime = sessionTime+300;
-      return ({...prevState, sessionTime, currentSessionTime: sessionTime})
-    })
-  }
+    setsessionDetail((prevState) => {
+      let { sessionTime } = prevState;
+      sessionTime = sessionTime + 300;
+      return { ...prevState, sessionTime, currentSessionTime: sessionTime };
+    });
+  };
 
   const sessionDecrementHandler = (e) => {
-    if(sessionDetail.sessionTime>300){
-      setsessionDetail((prevState)=>{
-        let {sessionTime} = prevState;
-        sessionTime = sessionTime-300;
-        return ({...prevState, sessionTime, currentSessionTime: sessionTime})
-      })
+    if (sessionDetail.sessionTime > 300) {
+      setsessionDetail((prevState) => {
+        let { sessionTime } = prevState;
+        sessionTime = sessionTime - 300;
+
+        return { ...prevState, sessionTime, currentSessionTime: sessionTime };
+      });
     }
-  }
+  };
 
   const breakIncrementHandler = () => {
-    setsessionDetail((prevState)=>{
-      let {breakTime} = prevState;
-      breakTime = breakTime+300;
-      return ({...prevState, breakTime, currentBreakTime: breakTime})
-    })
-  }
+    setsessionDetail((prevState) => {
+      let { breakTime } = prevState;
+      breakTime = breakTime + 300;
+      return { ...prevState, breakTime, currentBreakTime: breakTime };
+    });
+  };
 
   const breakDecrementHandler = () => {
-    if(sessionDetail.breakTime>300){
-      setsessionDetail((prevState)=>{
-        let {breakTime} = prevState;
-        breakTime = breakTime-300;
-        return ({...prevState, breakTime, currentBreakTime: breakTime})
-      })
+    if (sessionDetail.breakTime > 300) {
+      setsessionDetail((prevState) => {
+        let { breakTime } = prevState;
+        breakTime = breakTime - 300;
+        return { ...prevState, breakTime, currentBreakTime: breakTime };
+      });
     }
-  }
+  };
 
   const sessionStatusHandler = (e) => {
-    const {active} = sessionDetail;
+    const { active } = sessionDetail;
 
-    if(active==="false"){
-      const timer = setInterval(()=>{
-        setsessionDetail((prevState)=>{
-
+    if (active === "false") {
+      const timer = setInterval(() => {
+        setsessionDetail((prevState) => {
           // if the timers runs out then reset the timer.
-          if(prevState.active === "break"){
-            let {currentBreakTime} = prevState;
-            if(currentBreakTime===0){
-              return ({...prevState, currentBreakTime: prevState.breakTime, active: "session"});
-            }else{
-              currentBreakTime = currentBreakTime-1;
-              return ({...prevState, currentBreakTime, active: "break"});
+          if (prevState.active === "break") {
+            let { currentBreakTime } = prevState;
+            if (currentBreakTime === 0) {
+              soundElement.current.play();
+              return {
+                ...prevState,
+                currentBreakTime: prevState.breakTime,
+                active: "session",
+              };
+            } else {
+              currentBreakTime = currentBreakTime - 1;
+              return { ...prevState, currentBreakTime, active: "break" };
             }
-          }else{
-            let {currentSessionTime} = prevState;
-            if(currentSessionTime===0){
-              return ({...prevState, currentSessionTime: prevState.sessionTime, active: "break"});
-            }else{
-              currentSessionTime = currentSessionTime-1;
-              return ({...prevState, currentSessionTime, active: "session"});
+          } else {
+            let { currentSessionTime } = prevState;
+            if (currentSessionTime === 0) {
+              soundElement.current.play();
+              return {
+                ...prevState,
+                currentSessionTime: prevState.sessionTime,
+                active: "break",
+              };
+            } else {
+              currentSessionTime = currentSessionTime - 1;
+              return { ...prevState, currentSessionTime, active: "session" };
             }
           }
-        })
-          
+        });
       }, 1000);
       // toggle the state and set the interval id
-      setsessionDetail((prevState)=>{
-        return ({...prevState, timer})
-      })
+      setsessionDetail((prevState) => {
+        return { ...prevState, timer };
+      });
     } else {
-      setsessionDetail((prevState)=>{
+      setsessionDetail((prevState) => {
         clearInterval(prevState.timer);
-        return (initialSessionDetail);
-      })
+        return initialSessionDetail;
+      });
     }
-  }
+  };
 
   const getSeconds = (time) => {
-    let newTime = time%60;
-    newTime = newTime.toString().padStart(2, '0');
-    return (newTime);
-  }
-
-
+    let newTime = time % 60;
+    newTime = newTime.toString().padStart(2, "0");
+    return newTime;
+  };
 
   //JSX
 
   let title;
 
-  if(sessionDetail.active === "false"){
+  if (sessionDetail.active === "false") {
     title = "Start A Session";
-  }else if(sessionDetail.active === "session"){
+  } else if (sessionDetail.active === "session") {
     title = "Session is Running";
-  }else{
-    title = "Break Time!"
+  } else {
+    title = "Break Time!";
   }
-
-
 
   return (
     <Card
@@ -131,7 +148,18 @@ const PomTimer = () => {
         justifyContent: "space-around",
       }}
     >
-      <CardHeader title={<Typography variant="h4" align="center"> {title} </Typography>}/>
+      <audio ref={soundElement}>
+        <source src={soundFile} type="audio/wav" />
+      </audio>
+
+      <CardHeader
+        title={
+          <Typography variant="h4" align="center">
+            {" "}
+            {title}{" "}
+          </Typography>
+        }
+      />
       <Paper
         sx={{
           height: "10rem",
@@ -144,30 +172,64 @@ const PomTimer = () => {
         }}
       >
         <Typography variant="h4" sx={{ margin: "auto" }}>
-          {Math.trunc((sessionDetail.currentSessionTime)/60)+":"+getSeconds(sessionDetail.currentSessionTime)}
+          {Math.trunc(sessionDetail.currentSessionTime / 60) +
+            ":" +
+            getSeconds(sessionDetail.currentSessionTime)}
         </Typography>
-        <Button sx={{ width: "50%" }} onClick={sessionStatusHandler}>
-          {(sessionDetail.active==="false")?
-            <PlayArrowIcon fontSize="large" />:
-            <StopIcon fontSize="large" />
-          }
-          
-        </Button>
+        <Box>
+          <Button sx={{ width: "50%" }} onClick={sessionStatusHandler}>
+            {sessionDetail.active === "false" ? (
+              <PlayArrowIcon fontSize="large" />
+            ) : (
+              <StopIcon fontSize="large" />
+            )}
+          </Button>
+          {/* <Button>
+          <VolumeUpIcon />
+        </Button> */}
+        </Box>
+
         <Typography variant="captian" sx={{ margin: "auto" }}>
-        {Math.trunc((sessionDetail.currentBreakTime)/60)+":"+getSeconds(sessionDetail.currentBreakTime)}
+          {Math.trunc(sessionDetail.currentBreakTime / 60) +
+            ":" +
+            getSeconds(sessionDetail.currentBreakTime)}
         </Typography>
       </Paper>
-      
+
       <Box display="flex">
         <Box display="flex">
-          <Button className="decrement" disabled={sessionDetail.active!=="false"} onClick={sessionDecrementHandler}>-</Button>
+          <Button
+            className="decrement"
+            disabled={sessionDetail.active !== "false"}
+            onClick={sessionDecrementHandler}
+          >
+            -
+          </Button>
           <Typography>session time</Typography>
-          <Button className="increment" disabled={sessionDetail.active!=="false"} onClick={sessionIncrementHandler}>+</Button>
+          <Button
+            className="increment"
+            disabled={sessionDetail.active !== "false"}
+            onClick={sessionIncrementHandler}
+          >
+            +
+          </Button>
         </Box>
         <Box display="flex">
-          <Button className="decrement" disabled={sessionDetail.active!=="false"} onClick={breakDecrementHandler}>-</Button>
+          <Button
+            className="decrement"
+            disabled={sessionDetail.active !== "false"}
+            onClick={breakDecrementHandler}
+          >
+            -
+          </Button>
           <Typography>break time</Typography>
-          <Button className="increment" disabled={sessionDetail.active!=="false"} onClick={breakIncrementHandler}>+</Button>
+          <Button
+            className="increment"
+            disabled={sessionDetail.active !== "false"}
+            onClick={breakIncrementHandler}
+          >
+            +
+          </Button>
         </Box>
       </Box>
     </Card>
